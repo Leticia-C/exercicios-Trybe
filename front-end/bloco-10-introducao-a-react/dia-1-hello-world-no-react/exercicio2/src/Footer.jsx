ebdbc011c7ec93bfb4373bb99601828d79ec2f86
Leticia-C <leticia.camposg93@gmail.com>
2022-05-12 13:53:14 -0300

    Adiciona exercicios do bloco 10.1

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio2/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <footer>E isso é só o começo...</footer>
+        )
+    }
+}
+export default Footer
